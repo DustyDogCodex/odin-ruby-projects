ccf29f0ff52774e36790bf54aa94007d8b9f3ff2
Varun Malaviya <varunmalaviya7@gmail.com>
2022-03-18 22:34:17 -0400

    added case and when statements

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -2,8 +2,13 @@ def caesar_cipher(string, shift)
     str_array = string.split('')
     puts str_array
     cipher_array = str_array.map {|char| 
-      if (char.ord == 32) 
+      case char.ord
+      when char.ord.between?(31,64)
         char.ord
+      when char.ord - shift < 65
+        char.ord = 90 - (65 - (char.ord -shift))
+      when char.ord - shift < 97
+        char.ord = 122 - (97 - (char.ord -shift))
       else
         char.ord - shift
       end
@@ -14,4 +19,3 @@ def caesar_cipher(string, shift)
     cipher_str = cipher_array.join('')
     puts cipher_str
 end
-  
