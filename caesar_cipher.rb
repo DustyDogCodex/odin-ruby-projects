63998ecf7c66e62ca4d2b2e2f841e74a7b586a71
Varun Malaviya <varunmalaviya7@gmail.com>
2022-03-19 13:21:08 -0400

    modified algorithm for wrapping letters. Changed case to a better if/else block. Added comments to clarify each step

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,21 +1,21 @@
 def caesar_cipher(string, shift)
-    str_array = string.split('')
-    puts str_array
-    cipher_array = str_array.map {|char| 
-      case char.ord
-      when char.ord.between?(31,64)
-        char.ord
-      when char.ord - shift < 65
-        char.ord = 90 - (65 - (char.ord -shift))
-      when char.ord - shift < 97
-        char.ord = 122 - (97 - (char.ord -shift))
-      else
-        char.ord - shift
-      end
-    }
-    puts cipher_array
-    cipher_array = cipher_array.map {|num| num.chr}
-    puts cipher_array
-    cipher_str = cipher_array.join('')
-    puts cipher_str
+  str_array = string.split('')
+  
+  ord_array = str_array.map {|char| char.ord}
+    
+  cipher_array = ord_array.map {|value| 
+    #selecting an appropriate base for given letters. 65 for capital letters and 97 for lowercase letters.
+    base = value < 91 ? 65 : 97
+    if value.between?(65,90) || value.between?(97,122)
+      #algo for shifting wrapping between (z and a) or (Z and A)
+      switch = (((value - base) + shift) % 26) + base   
+    else
+      #returning space and special characters as they were
+      value
+    end
+  }
+  #converting ordinal ASCII values back to characters
+  cipher_array = cipher_array.map {|num| num.chr}
+  #final cipher formed by adding the characters in cipher_array
+  cipher_str = cipher_array.join('')
 end
